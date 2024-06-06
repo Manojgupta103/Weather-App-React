f96573a325f7d2d8640ef386d2718d5687b1b134
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-07 00:45:19 +0530

    chore: Add weather data fetching and display functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import './App.css'
 function App() {
 
   let [city, setCity]=useState('')
+  let[wdetails, setWdetails]=useState()
   let getData=(event)=>{
+    fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${city}&apikey=iTtJ7WmhniemahrZjeVFdpY0pTyPGEoK`)
+      .then((res) => res.json())
+      .then((finalRes) => {
+        if(finalRes.cod=="404"){
+          setWdetails(undefined)
+        }
+        else
+        {
+          setWdetails(finalRes)
+        }
+      })
+
     event.preventDefault()
+    setCity('')
   }
 return(
   <div className='w-[100%] h-[100vh] bg-[#4aacb1]' >
@@ -19,11 +33,22 @@ return(
     </form>
 
 
-  <div className='w-[400px] mx-auto bg-white shadow-lg mt-[200px] p-[25px]'>
-  <h3 className='font-bold text-[30px] text-black' >Jaipur<span className='bg-[yellow]'>IN</span></h3>
-  <h3 className='font-bold text-[30px] text-black'>9.62oc</h3>
-  <img src='http://openweathermap.org/img/w/50d.png' />
-  <p>Fog</p>
+  <div className='w-[400px] mx-auto bg-[lightblue] shadow-lg mt-[200px] p-[25px]'>
+  
+  {wdetails!==undefined
+  ?
+  <>
+    <h3 className='font-bold text-[30px] text-black' >Jaipur<span className='bg-[yellow]'> IN </span></h3>
+    <h3 className='font-bold text-[30px] text-black'>
+      {wdetails.main.temp}
+    </h3>
+    <img src='http://openweathermap.org/img/w/50d.png' />
+    <p className='text-black'>Fog</p>
+  </>
+  :
+  "No Data Found"
+  }
+  
     </div> 
   </div>
   </div>
